f02a9d9f2a7b04e5c54eaedc043333e2914929ca
Levrk <levrolandkalb@gmail.com>
2023-09-06 13:06:20 -0400

    Completed CQ1

---
+++ CQ1/Staircase/Solution.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n) {
+    String spaces = " ".repeat(n-1);
+    
+    for (int i = 1; i <= n ;i++){
+        System.out.print(spaces);    
+        if (spaces!="") {
+            spaces = spaces.substring(0,spaces.length()-1);
+        } 
+        System.out.println("#".repeat(i));
+    }
+    
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.staircase(n);
+
+        bufferedReader.close();
+    }
+}
