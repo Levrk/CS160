200482b36082a4fffb693a4460cbbd15d3cea2ad
Levrk <levrolandkalb@gmail.com>
2023-09-13 12:51:40 -0400

    completed cq2

---
+++ CQ2/MarsExploration/Solution.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'marsExploration' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int marsExploration(String s) {
+        String compare = "SOS".repeat(33);
+        int count = 0;
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) != compare.charAt(i)){
+                count += 1;
+                System.out.println(s.charAt(i));
+            }
+        }
+        return count;
+        
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.marsExploration(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
