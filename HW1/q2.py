7f6ad2638afa9fbc14d2bbc201ccdde24d10ca10
Levrk <levrolandkalb@gmail.com>
2023-09-15 11:45:40 -0400

    fixed time complexity testing

--- HW1/q2.py
+++ HW1/q2.py
@@ -37,8 +37,10 @@ l4 = [2,4,6,8,10,12,14,16,18,20]
 l5 = []
 
 t1 = [i for i in range(1, 11)]
-t2 = [j for j in range(1, 5000001)]
-t3 = [k for k in range(1, 10000001)]
+t2 = [j for j in range(1, 250001)]
+t3 = [k for k in range(1, 500001)]
+t4 = [l for l in range(1, 750001)]
+t5 = [m for m in range(1, 1000001)]
 
 
 
@@ -69,25 +71,35 @@ print(f"10: {time1:.6f} seconds")
 
 
 time2 = timeit.timeit(lambda: binSearch(t2, 0), number=100)
-print(f"5000000: {time2:.6f} seconds")
+print(f"250000: {time2:.6f} seconds")
 
 time3 = timeit.timeit(lambda: binSearch(t3, 0), number=100)
-print(f"10000000: {time3:.6f} seconds")
+print(f"500000: {time3:.6f} seconds")
+
+time4 = timeit.timeit(lambda: binSearch(t4, 0), number=100)
+print(f"750000: {time4:.6f} seconds")
+
+time5 = timeit.timeit(lambda: binSearch(t5, 0), number=100)
+print(f"1000000: {time5:.6f} seconds")
 
 """
 Sample results of time complexity testing:
 
-execution time for 100 function calls with a list of length:
-10: 0.000163 seconds
-5000000: 0.000897 seconds
-10000000: 0.001112 seconds
+10: 0.000100 seconds
+2500000: 0.000392 seconds
+5000000: 0.000443 seconds
+7500000: 0.000592 seconds
+10000000: 0.000675 seconds
 
 These results align with the expected outcome. The function binSearch should be executed in log time, 
 meaning results will grow at a slower rate as the size of the input increases. Our results indicate
-that the functions execution time increased by 5.5 times between inputs of size 10 and 5 million. 
-The growth then increased by a factor of only 1.2 between the input of size 5 million and ten million. 
+that the functions execution time increased by 4.43 times between inputs of size 10 and .5 million. 
+The growth then increased by a factor of only 1.52 between the input of size .5 million and 1 million. 
 This aligns with our expectations because it shows that the functions time complexity grows quickly at first,
-but eventually slows down with larger inputs.
+but eventually slows down with larger inputs. We can see this even more clearly by plotting the points generated
+by this time complexity analysis on a graph along side a logarithmic equation y = .0001log_{2.7}(x)-.001. 
+We can see from the graph that the time taken to execute these function calls increases logarithmically,
+which is what we expect.
 
 """
 
