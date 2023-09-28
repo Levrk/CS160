b7d73cddb00c6a9efb72d3216ec365d99c9a04be
Levrk <levrolandkalb@gmail.com>
2023-09-28 14:30:30 -0400

    updated q1

--- HW2/q1.py
+++ HW2/q1.py
@@ -11,41 +11,52 @@
 # our data structure is a list of spaces 
 
 class Node:
-    def __init__(self, left = None,right = None, list = None):
+    def __init__(self, left = None,right = None, points = None, name=None):
         #left and right can be either nodes or leafs
-        self.left = None
-        self.right = None
+        self.left = left
+        self.right = right
         #list is the list of excluded leaves
-        self.list = []
-
-class Leaf:
-    def __init__(self, points = None):
-        #list of tuples representing the points on the edge of the 2d space
-        self.points = []
-        
-
-
-L1 = Leaf([(1,-1),(1,1),(1,0.8),((-1,0.8))])
-
-N1 = Node(None,None,[L1.points])
-
-HN = Node(L1, N1, [])
+        self.points = points
+        self.name = name
 
 
 def makeTree(y):
-    L1 = Leaf([(1,-1),(1,1),(1,y),((-1,y))])
-    L2 = Leaf()
-    L3 = Leaf()
-    L4 = Leaf()
-    L5 = Leaf()
-    L6 = Leaf()
-    L7 = Leaf()
-    L8 = Leaf()
+    third = 4/3
+    L1 = Node(None, None, [((-2*y),(y)),   ((-2*y),(third * y)),   ((2*y),(y)),    ((2*y),(third * y))],"Leaf 1")
+    L2 = Node(None, None, [((-2*y),(y)),   ((-1.5*y),(y)),((-2*y),   (-third * y)),((-1.5*y),   (-third * y))],"Leaf 2")
+    L3 = Node(None, None, [((-2*y),(y)),   ((1.3*y),(y)),    ((-2*y),(-third * y)),    ((1.3*y),(-third * y))],"Leaf 3")
+    L4 = Node(None, None, [((-1.5*y),(-y)),   ((-1.5*y),(-third * y)),    ((1.3*y),(-y)),    ((1.5*y),(-third * y))],"Leaf 4")
+    L5 = Node(None, None, [((-1.5*y),y),    ((-.4*y),y),     ((-1.5*y),-y),    ((-.4*y),-y)],"Leaf 5")
+    L6 = Node(None, None, [((-.4*y),0),   ((1.3*y),0),    ((-.4*y),-y),     ((1.3*y),-y)],"Leaf 6")
+    L7 = Node(None, None, [((-.4 * y),(.9 * y)),    ((0.32 * y), 0),    ((-.4 * y),y)   ,((1.3*y),y),   ((1.3*y),0) ],"Leaf 7")
+    L8 = Node(None, None, [((-.4 * y),(.9 * y)),    ((0.32 * y), 0),    ((-.4 * y), 0)],"Leaf 8")
 
-    N6 = Node(None,None,[L1.points,L2.points,L3.points,L4.points, L5.points,L6.points])
-    N5 = Node(None,None,[L1.points,L2.points,L3.points,L4.points, L5.points])
-    N4 = Node(None,None,[L1.points,L2.points,L3.points,L4.points])
-    N3 = Node(None,None,[L1.points,L2.points,L3.points])
-    N2 = Node(None,None,[L1.points,L2.points])
-    N1 = Node(None,None,[L1.points])
-    HN = Node(L1, N1, [])
+    N6 = Node(L7,L8,[],"Node 6")
+    N5 = Node(N6,L6,[],"Node 5")
+    N4 = Node(L5,N5,[],"Node 4")
+    N3 = Node(N4,L4,[],"Node 3")
+    N2 = Node(N3,L3,[],"Node 2")
+    N1 = Node(L2,N2,[],"Node 1")
+    HN = Node(L1, N1, [],"Head Node")
+    return HN
+
+
+def traverse(node):
+    #verify the argument provided is not None
+    if node is not None:
+        #recursively traverse the left child
+        traverse(node.left)
+
+        #traverse the current node
+        if node.points != []:
+            #print(node.name + " : " + str(node.points))
+            print("This is " + node.name)
+        else:
+            print("This is " + node.name)
+
+        #recursively traverse the right child
+        traverse(node.right)
+
+test = makeTree(1.2)
+
+traverse(test)
